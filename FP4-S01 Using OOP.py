367f830a9b35c438b848a3b1c215a3bc0b4cb69f
BenjaminWalz <92549207+BenjaminWalz@users.noreply.github.com>
2021-12-08 00:58:23 -0600

    Second commit
    Added a new player class and plan on making inherit from the Team class and uses that information in the player's profile.

--- FP4-S01 Using OOP.py
+++ FP4-S01 Using OOP.py
@@ -1,23 +1,35 @@
 class Team:
     
-    def __init__(self, team_name, location, wins, losses, ties,):
-        self.team_name = team_name
+    def __init__(self, team, location, wins, losses, ties,):
+        self.team = team
         self.location = location
         self.wins = wins
         self.losses = losses
         self.ties = ties
    
     def Team_Location(self):
-        return '{} {}'.format(self.location, self.team_name)
+        return '{} {}'.format(self.location, self.team)
         
     def Record(self):
-        return '{} {} {}'.format(self.wins, self.losses, self.ties)
+        self.record = self.wins, self.losses, self.ties
         
-        
-team = Team('Wheat Kings', 'West Central', '10,', '8,', '2')
+      
+      
+class Player(Team):
+    def __init__(self, team, number, wins, losses, ties):
+        super().__init__(team, wins, losses, ties)
+      # this above dont work ben
+      
+      
+      
+      
+      
+team = Player('Wheat Kings', 'West Central', 10, 8, 2)
 
 
 print(team.Team_Location())
 print(team.Record())
+
+
         
         
