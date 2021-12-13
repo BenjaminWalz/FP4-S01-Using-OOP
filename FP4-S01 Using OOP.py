cb38cfac437b74dabfa2d30a575aae2629a77a58
BenjaminWalz <92549207+BenjaminWalz@users.noreply.github.com>
2021-12-12 23:36:08 -0600

    second commit
    made it have a user input to determind the players info, need to do the same for team. need to make more options to do.

--- FP4-S01 Using OOP.py
+++ FP4-S01 Using OOP.py
@@ -11,24 +11,55 @@ class Team:
         return '{} {}'.format(self.location, self.team)
         
     def Record(self):
-        self.record = self.wins, self.losses, self.ties
+        return '{} {} {}'.format(self.wins, self.ties, self. losses)
         
       
       
 class Player(Team):
-    def __init__(self, team, number, wins, losses, ties):
-        super().__init__(team, wins, losses, ties)
-      # this above dont work ben
-      
-      
-      
-      
-      
-team = Player('Wheat Kings', 'West Central', 10, 8, 2)
+    
+    def __init__(self, team, first, last, number, wins, losses, ties, goals, assists):
+        super().__init__(self, team, wins, losses, ties)
+        self.team = team
+        self.number = number
+        self.first = first
+        self.last = last
+        self.goals = goals
+        self.assists = assists
+        self.points = goals + assists
+          
+    def Player_Profile(self):
+        return '{} {} {} {}'.format(self.first, self.last, self.number, self.points)
+    
+ 
+ 
+# location = input("Where is the team from? ")
+# team = input("What is the team name? ")
+# team = Team(team, location, 0, 0, 0)
+# print(team.Team_Location())
+ 
+ 
 
+def main():
+    print("""
+This program makes and groups either a team with the teams wins, ties and losses,
+or a player with they're name, number, goals and assits with total points.
+""")
+    choice = int(input("Do you want to group a player(1) or a team(2) "))
+    if choice == 1:
+        print("list the following info")
+        team = input("What is the team name> ")
+        first = input("What is the players first name> ")
+        last = input("What is the players last name> ")
+        number = input("What number is the player> ")
+        number = '#' + number
+        goals = int(input("How many goals does your player have> "))
+        assists = int(input("How many assists does your player gave> "))
+        player = Player(team, first, last, number, 0, 0, 0, goals, assists)
+        print(player.Player_Profile())
+        
+main()
+        
 
-print(team.Team_Location())
-print(team.Record())
 
 
         
