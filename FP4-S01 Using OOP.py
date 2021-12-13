99fa6e3b39a9fc0246a1b7cd609bf2e1279f5d24
BenjaminWalz <92549207+BenjaminWalz@users.noreply.github.com>
2021-12-13 16:48:41 -0600

    third commit
    made its own interface to add players to teams, need to figure out how to add info into a dict key. need to make more options with class functions and have more options for the interface for the user to use.

--- FP4-S01 Using OOP.py
+++ FP4-S01 Using OOP.py
@@ -26,17 +26,15 @@ class Player(Team):
         self.goals = goals
         self.assists = assists
         self.points = goals + assists
-          
+        self.name = first + ' ' + last
     def Player_Profile(self):
-        return '{} {} {} {}'.format(self.first, self.last, self.number, self.points)
+        return '{} {} {} {} {}'.format(self.name, self.number, self.goals, self.assists, self.points)
     
  
  
-# location = input("Where is the team from? ")
-# team = input("What is the team name? ")
-# team = Team(team, location, 0, 0, 0)
-# print(team.Team_Location())
- 
+
+roster = {}
+teams = {}
  
 
 def main():
@@ -44,18 +42,59 @@ def main():
 This program makes and groups either a team with the teams wins, ties and losses,
 or a player with they're name, number, goals and assits with total points.
 """)
-    choice = int(input("Do you want to group a player(1) or a team(2) "))
-    if choice == 1:
-        print("list the following info")
-        team = input("What is the team name> ")
-        first = input("What is the players first name> ")
-        last = input("What is the players last name> ")
-        number = input("What number is the player> ")
-        number = '#' + number
-        goals = int(input("How many goals does your player have> "))
-        assists = int(input("How many assists does your player gave> "))
-        player = Player(team, first, last, number, 0, 0, 0, goals, assists)
-        print(player.Player_Profile())
+    go = True
+    while go == True:
+        choice = input("Do you want to group a player(1) team(2) or exit(3) ")
+        if choice == '1':
+            print("list the following info")
+            team = input("What is the team name> ")
+            first = input("What is the players first name> ")
+            last = input("What is the players last name> ")
+            number = input("What number is the player> ")
+            number = '#' + number
+            goals = int(input("How many goals does your player have> "))
+            assists = int(input("How many assists does your player have> "))
+            points = goals + assists
+            player = Player(team, first, last, number, 0, 0, 0, goals, assists)
+            add = input("Do you want to add this player to a roster? Yes/No ")
+            if add == 'Yes' or add == 'yes':
+                name = first + ' ' + last
+                stats = player.Player_Profile()
+                if team in roster:
+                    roster.add(stats)
+                    print(roster)
+                    
+                else:
+                    roster[team] = stats
+                    print("You added", name)
+                    print(roster)
+            else:
+                print(player.Player_Profile())
+            
+        elif choice == '2':
+            print("list the following info")
+            team = input("What is the name of the team> ")
+            location = input("Where is the team from> ")
+            wins = input("How many wins does the team have> ")
+            losses = input("How many losses does the team have> ")
+            ties = input("How many ties does the team have> ")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        elif choice == '3':
+            print("Thank you, goodbye")
+            go = False
+        else:
+            print("That's not an option")
         
 main()
         
